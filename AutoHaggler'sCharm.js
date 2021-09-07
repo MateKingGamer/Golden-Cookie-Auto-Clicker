93d87ec07e7a26e2e288a34b43c2b0919a872b28
MateKingGamer <80847780+MateKingGamer@users.noreply.github.com>
2021-09-07 22:05:53 +0200

    Update AutoHaggler'sCharm.js

--- AutoHaggler'sCharm.js
+++ AutoHaggler'sCharm.js
@@ -1,3 +1,4 @@
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
 var mana = 100,
     spell = "haggler's charm",
     grimoire = Game.Objects["Wizard tower"].minigame;
