09e2734fb55b903030b26a1afe9ed084857be4e6
MateKingGamer <80847780+MateKingGamer@users.noreply.github.com>
2021-09-07 22:07:08 +0200

    Delete AutoHaggler'sCharm.js

--- AutoHaggler'sCharm.js
+++
@@ -1,9 +0,0 @@
-if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
-var mana = 100,
-    spell = "haggler's charm",
-    grimoire = Game.Objects["Wizard tower"].minigame;
-function autocastSpell() {
-  if (grimoire.magic >= mana)
-    grimoire.castSpell(grimoire.spells[spell]);
-}
-setInterval(autocastSpell, 1000);
